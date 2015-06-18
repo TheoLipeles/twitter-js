a6df417e526709d510f42fea908edf65c37ae5d8
Christian Evans <christian.evans214@gmail.com>
2015-06-17 22:09:15 -0400

    sockets working! How do we close them?

--- routes/index.js
+++ routes/index.js
@@ -34,14 +34,14 @@ var tweetBank = require('../tweetBank');
 module.exports = function(io){
   router.get('/', function (req, res) {
     var tweets = tweetBank.list();
-    res.render( 'index', { title: 'Twitter.js', tweets: tweets, seeMore: "See More", showForm: true } );
+    res.render( 'index', { title: 'Twooter.js', tweets: tweets, seeMore: "See More", showForm: true } );
   });
 
   router.get('/users/:name', function(req, res) {
     var name = req.params.name;
     var tweets = tweetBank.find( {name: name} );
     //res.send(list);
-    res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: tweets, seeMore: "See More", showForm: true, Name: name} );
+    res.render( 'index', { title: 'Twooter.js - Posts by '+name, tweets: tweets, seeMore: "See More", showForm: true, Name: name} );
   });
 
   router.get('/users/:name/tweets/:id', function(request, response) {
@@ -54,7 +54,8 @@ module.exports = function(io){
   router.post('/submit', function(request, response){
     var id = tweetBank.add(request.body.name, request.body.text);
     var tweets = tweetBank.find( {id: id} );
-    io.sockets.emit('new_tweet', tweets)
+    console.log(tweets);
+    io.sockets.emit('new_tweet', tweets);
 
     //response.render( 'index', { title: "New post by " + request.body.name, tweets: tweets, seeMore: "", showForm: false } );
     })
